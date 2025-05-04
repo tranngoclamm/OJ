eb36e5e01ec1f84d8178208ee973ac058de9086f
tranngoclamm <124114520+tranngoclamm@users.noreply.github.com>
2025-05-04 12:29:55 +0000

    fix(organization): fix add admin to group

--- judge/utils/organization.py
+++ judge/utils/organization.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import Group
 def add_admin_to_group(form):
     org = form.save()
     all_admins = org.admins.all()
-    g = Group.objects.get(name=settings.GROUP_PERMISSION_FOR_ORG_ADMIN)
+    g, _ = Group.objects.get_or_create(name=settings.GROUP_PERMISSION_FOR_ORG_ADMIN)
     for admin in all_admins:
         admin.user.groups.add(g)
